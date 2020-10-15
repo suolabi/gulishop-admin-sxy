3edd7e324633fb8d3f6f84bca35754404cbe1098
suolabi <suolabi@163.com>
2020-10-15 21:44:41 +0800

    10.15

---
+++ src/api/product/trademark.js
@@ -0,0 +1,20 @@
+import request from '@/utils/request'
+
+
+
+export default {
+    // 我们要一次把增删改查的接口全部写完
+    // DELETE /admin/product/baseTrademark/remove/{id}
+    // 删除BaseTrademark
+    
+    // POST /admin/product/baseTrademark/save
+    // 新增BaseTrademark
+    
+    // PUT /admin/product/baseTrademark/update
+    // 修改BaseTrademark
+    
+    // GET /admin/product/baseTrademark/{page}/{limit}
+    // 分页列表
+    
+
+}
