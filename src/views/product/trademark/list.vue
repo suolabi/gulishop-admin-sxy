ea5b34940a862fb518ce1e9a118f27c9f7087831
suolabi <suolabi@163.com>
2020-10-23 00:01:31 +0800

    10.23

--- src/views/product/trademark/list.vue
+++ src/views/product/trademark/list.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <el-button type="primary" icon="el-icon-plus" @click="showAddDialog">添加</el-button>
+    <el-button type="primary" icon="el-icon-plus" @click="showAddDialog"
+      >添加</el-button
+    >
     <!--
       参考官网最下边的属性
       data:接收动态数据，给到下边的每一个el-table-column列
@@ -47,10 +49,18 @@
       <el-table-column prop="prop" label="操作" width="width">
         <!-- 这里也是内部有其他结构了，就一个需要作用域插槽 -->
         <template slot-scope="{ row, $index }">
-          <el-button type="warning" icon="el-icon-edit" size="mini"
+          <el-button
+            type="warning"
+            icon="el-icon-edit"
+            size="mini"
+            @click="showUpdateDialog(row)"
             >修改</el-button
           >
-          <el-button type="danger" icon="el-icon-delete" size="mini"
+          <el-button
+            type="danger"
+            icon="el-icon-delete"
+            size="mini"
+            @click="deleteTrademark(row)"
             >删除</el-button
           >
         </template>
@@ -71,15 +81,18 @@
 
     <!--增加和修改的dialog -->
     <!-- :visible.sync  用来控制点击叉号关闭 -->
-    <el-dialog title="添加品牌" :visible.sync="isShowDialog">
+    <el-dialog
+      :title="form.id ? '修改品牌' : '添加品牌'"
+      :visible.sync="isShowDialog"
+    >
       <!-- :model="form"  这里要接收一个对象，下边所有收集到的信息都存入这个对象中 -->
-      <el-form :model="form" style="width: 80%">
+      <el-form :model="form" :rules="rules" style="width: 80%" ref="form">
         <!-- :label-width： 控制显示名称的宽度 -->
-        <el-form-item label="品牌名称" :label-width="'100px'">
+        <el-form-item label="品牌名称" :label-width="'100px'" prop="tmName">
           <!-- v-model:用来收集表单数据 -->
           <el-input v-model="form.tmName" autocomplete="off"></el-input>
         </el-form-item>
-        <el-form-item label="品牌LOGO" :label-width="'100px'">
+        <el-form-item label="品牌LOGO" :label-width="'100px'" prop="logoUrl">
           <!-- 
             action:上传文件的路径，注意跨域问题
             :show-file-list：是否展示多张图片，上传多张能用到
@@ -115,6 +128,17 @@
 export default {
   name: "Trademark",
   data() {
+    // callback   验证通过和不同都是由这个函数来决定的  
+    var validateTmName = (rule, value, callback) => {
+      if(value.length < 2 || value.length > 10 ){
+        callback(new Error('输入的名字必须是2-10个字符'))//不通过
+      }else if(value.trim() === ''){
+        callback(new Error('请输入合法的名称不能为空'))  //不通过
+      }else{
+        callback() //通过
+      }
+    };
+
     return {
       page: 1,
       limit: 3,
@@ -125,7 +149,35 @@ export default {
 
       form: {
         tmName: "",
-        logoUrl:''
+        logoUrl: "",
+      },
+
+      rules: {
+        tmName: [
+          // required: true   必填项
+          // message          错误信息
+          // trigger          验证规则的触发时机
+          // blur      失去焦点的时候验证
+          // change    内容改变的时候验证
+          // 还有一个验证是点确定按钮整体验证
+          // min              输入的最小字符
+          // max              输入的最大字符
+          { required: true, message: "请输入品牌名称", trigger: "blur" },
+          // {
+          //   min: 2,
+          //   max: 10,
+          //   message: "长度在 2 到 10 个字符",
+          //   trigger: "change",
+          // },
+
+          { validator: validateTmName, trigger: "change" },
+
+          // { min: 2, max: 10, message: "长度在 2 到 10 个字符", trigger: "change" },
+        ],
+        logoUrl: [
+          // trigger: 'change' 对upload来说看不到这个效果，必须是整体验证的时候才能有验证的错误提示
+          { required: true, message: "请上传logo图片", trigger: "change" },
+        ],
       },
     };
   },
@@ -150,13 +202,24 @@ export default {
       this.getTrademarkList();
     },
 
+    // 点击添加按钮
+    showAddDialog() {
+      (this.isShowDialog = true),
+        (this.form = {
+          tmName: "",
+          logoUrl: "",
+        });
+    },
 
-    showAddDialog(){
-      this.isShowDialog = true,
-      this.form={
-        tmName: "",
-        logoUrl:''
-      }
+    // 点击修改按钮
+    showUpdateDialog(row) {
+      this.isShowDialog = true;
+      //row是trademarkList里面的一个对象，用来展示在页面上的
+      //我们把row的地址给了form一份   row和form就指向同一个对象，以后修改form就是在修改row,row改了，页面就会改
+      // this.form = row
+      // 这时候我们不能使用等号赋值的形式了，需要使用拷贝
+      // form里都是基本数据类型，使用浅拷贝就可以了
+      this.form = { ...row };
     },
 
     //上传成功的回调函数
@@ -190,27 +253,57 @@ export default {
       return isJPG && isLt2M;
     },
 
-
     //点击确定按钮添加或者修改trademark的请求逻辑
-    async addOrUpdateTrademark(){
-      // 获取参数
-      let trademark = this.form
-      // 整理参数 ===》最麻烦
-      // 发请求
-      const result = await this.$API.trademark.addOrUpdate(trademark)
-      // 成功干啥
-      if(result.code === 200){
-        //1、提示添加或者修改成功
-        this.$message.success('添加品牌车成功')
-        //2、关闭dialog
-        this.isShowDialog = false;
-        //3、重新获取列表数据展示
-        this.getTrademarkList();
-      }else{
-      // 失败干啥
-      this.$message.error('添加品牌失败')
-      }
-    }
+    addOrUpdateTrademark() {
+      // 验证规则
+      // valid：代表验证成功还是失败
+      this.$refs.form.validate(async (valid) => {
+        if (valid) {
+          // 验证成功才能有后续
+          // 获取参数
+          let trademark = this.form;
+          // 整理参数 ===》最麻烦
+          // 发请求
+          const result = await this.$API.trademark.addOrUpdate(trademark);
+          // 成功干啥
+          if (result.code === 200) {
+            //1、提示添加或者修改成功
+            this.$message.success(`${trademark.id ? "修改" : "添加"}品牌成功`);
+            //2、关闭dialog
+            this.isShowDialog = false;
+            //3、重新获取列表数据展示
+            // （1）如果是添加我们默认是添加在最后一页，重新获取数据也是默认拿的是第一页
+            // （2）但是修改重新获取数据也应该是获取修改的那一页的数据
+            // 还是判断id是否存在，如果存在，把当前页码传进去，如果不存在，就默认把第一页传进去
+            this.getTrademarkList(trademark.id ? this.page : 1);
+          } else {
+            // 失败干啥
+            this.$message.error(`${trademark.id ? "修改" : "添加"}品牌失败`);
+          }
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+
+    deleteTrademark(row) {
+      this.$confirm(`你确定删除${row.tmName}吗？`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(async () => {
+          const result = await this.$API.trademark.delete(row.id);
+          this.getTrademarkList(
+            this.trademarkList.length > 1 ? this.page : this.page - 1
+          );
+          this.$message.success("删除成功");
+        })
+        .catch(() => {
+          this.$message.info("取消删除");
+        });
+    },
   },
 };
 </script>
